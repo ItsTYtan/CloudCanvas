3c69fa5217dabc01e344c46b2eaae73513f3e0c7
ItsTYtan <tytan216@gmail.com>
2023-11-14 17:48:49 +0800

    Add bare html

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const User = require('./model/user');
 const File = require('./model/file');
+require('dotenv').config();
 
 const app = express();
 
-const dbUrl = 
-mongoose.connect(dbUrl)
-.then((res) => app.listen(3000))
-.catch(e => console.log(e));
+main();
 
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
+async function main() {
+    const dbUrl = mongoose.connect(process.env.DB_URL)
+    .then((res) => app.listen(3000))
+    .catch(e => console.log(e));
 
-app.get('/', (req, res) => {
-    res.render('index');
-})
+    const user = await User.findOne({canvas_api_key: process.env.CANVAS_API_KEY})
+        .populate('courses')
+        .exec();
+    console.log(user);
+
+    app.set('view engine', 'ejs');
+    app.use(express.static('public'));
+
+    app.get('/', (req, res) => {
+        res.render('index', user);
+    });
+
+    app.get('/course/:id', (req, res) => {
+        res.render('course', user.courses);
+    })
+}
