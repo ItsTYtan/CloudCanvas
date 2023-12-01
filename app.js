44713d7ba9ac045f66784037aa538f723c8f863a
ItsTYtan <tytan216@gmail.com>
2023-12-01 17:38:59 +0800

    Add barebones course details

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const User = require('./model/user');
-const File = require('./model/file');
+const Course = require('./model/course');
 require('dotenv').config();
 
 const app = express();
@@ -9,14 +9,12 @@ const app = express();
 main();
 
 async function main() {
-    const dbUrl = mongoose.connect(process.env.DB_URL)
+    await mongoose.connect(process.env.DB_URL)
     .then((res) => app.listen(3000))
     .catch(e => console.log(e));
 
     const user = await User.findOne({canvas_api_key: process.env.CANVAS_API_KEY})
-        .populate('courses')
-        .exec();
-    console.log(user);
+        .populate('courses');
 
     app.set('view engine', 'ejs');
     app.use(express.static('public'));
@@ -26,6 +24,11 @@ async function main() {
     });
 
     app.get('/course/:id', (req, res) => {
-        res.render('course', user.courses);
+        Course.findById(req.params.id)
+        .populate('folders')
+        .then(course => {
+            console.log(course);
+            res.render('course', course);
+        });
     })
 }
